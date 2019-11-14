db722fa057a343681490df29b7592485849b6078
Lukas Corbitt <movalley5@gmail.com>
2019-11-14 12:29:46 -0700

    Write RSpec test

---
+++ spec/hello_world_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+ 
+RSpec.describe 'Hello world', type: :feature do
+  scenario 'index page' do
+    visit hello_world_index_path
+    expect(page).to have_content('Hello, world!')
+  end
+end
